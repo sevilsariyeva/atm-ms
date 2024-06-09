055c9976deeb1194abe864e8584bf261bfa36e0f
sevilsariyeva00 <sevilsariyeva00@gmail.com>
2024-06-09 15:17:52 +0400

    Account class fixed

--- src/main/java/com/example/atmbackend_ms/model/Account.java
+++ src/main/java/com/example/atmbackend_ms/model/Account.java
@@ -1,7 +1,6 @@
 package com.example.atmbackend_ms.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +11,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "accounts")
 public class Account {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String cardNumber;
 
     private int pin;

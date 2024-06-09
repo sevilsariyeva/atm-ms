20b3170222d54f55e11d2ae33a433ec1cb835971
sevilsariyeva00 <sevilsariyeva00@gmail.com>
2024-06-09 16:14:56 +0400

    SecurityConfig added

---
+++ src/main/java/com/example/atmbackend_ms/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.atmsimulation.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf->csrf.disable())// Disabling CSRF protection
+                .authorizeRequests(authorize->authorize
+                        .requestMatchers("/atm/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
